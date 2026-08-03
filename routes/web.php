163a9b9caf821b461b10e4a48a65861051e12445
Mohammad Akbarzadeh <moho.akbarzadeh@gmail.com>
2026-08-03 08:47:36 +0330

    redirect to dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => redirect()->route('dashboard'));
 
 Route::get('/dashboard', function () {
     return view('dashboard');
