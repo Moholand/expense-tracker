47985db9fa5cf64f3cc18327fb6468dc9d82539e
Mohammad Akbarzadeh <moho.akbarzadeh@gmail.com>
2026-08-22 20:19:34 +0330

    fix issue in click selected class

--- resources/views/livewire/categories.blade.php
+++ resources/views/livewire/categories.blade.php
@@ -76,10 +76,10 @@
 
                         <div class="form-group">
                             <label>Color</label>
-                            <div class="color-picker" x-data="{ selected: @js($color) }">
+                            <div class="color-picker" x-data="{ selected: $wire.entangle('color').live }">
                                 @foreach($colorOptions as $colorName => $colorOption)
-                                    <label class="color-option" :class="selected === @js($colorName) ? 'selected' : ''" @click="selected = @js($colorName); $wire.set('color', @js($colorName))">
-                                        <input type="radio" :checked="selected === @js($colorName)" value="{{ $colorName }}" class="sr-only">
+                                    <label class="color-option" :class="selected === @js($colorName) ? 'selected' : ''">
+                                        <input type="radio" x-model="selected" value="{{ $colorName }}" class="sr-only">
                                         <span class="color-swatch" style="background-color: {{ $colorOption['bg'] }}; border-color: {{ $colorOption['color'] }};">
                                             <span class="color-dot" style="background-color: {{ $colorOption['color'] }};"></span>
                                         </span>
